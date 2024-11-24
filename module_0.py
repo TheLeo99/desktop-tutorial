d3600e5d9636df3b029c9d346ede01a608e9b87e
TheLeo99 <leo.shestoff@gmail.com>
2024-11-24 13:09:08 +0300

    Create module_0.py

---
+++ module_0.py
@@ -0,0 +1,16 @@
+#task_1
+print(9 ** 0.5 * 5)
+
+
+#task_2
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+
+#task_3
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+
+#task_4
+print(int(float('123.456') * 10) % 10)
